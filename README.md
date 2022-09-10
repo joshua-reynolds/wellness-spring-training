69fd55ea3f7bc14011bb28a70d924ddaf15b667c
unknown <jdreynolds33@gmail.com>
2022-09-10 16:11:48 -0600

    dusting off the cobwebs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # wellness-spring-training
+
+Following these tutorials:
+- https://medium.com/codeburst/how-to-style-your-website-with-css-e72e7046fda5
+- https://medium.com/@blondiebytes/how-to-create-interactive-websites-with-javascript-627a6d998fed
+- https://medium.com/@blondiebytes/get-your-website-on-the-web-with-github-pages-9bdc2886ae7a
---
+++ js/script.js
@@ -0,0 +1,43 @@
+/* 
+function revealMessage() {
+	document.getElementById("hiddenMessage").style.display = 'block';
+}
+
+
+
+function countDown(){
+	var currentVal = document.getElementById("countDownButton").innerHTML;
+	var newVal = 0;
+
+	if (currentVal > 0){
+		newVal = currentVal - 1;
+	}
+	document.getElementById('countDownButton').innerHTML = newVal;
+}
+*/
+
+function revealMessage2() {
+	document.getElementById("hiddenMessage2").style.display = 'block';
+}
+
+function play() {
+	var audio = new Audio("sound_effects/8d82b5_Baseball_Hit_Sound_Effect.mp3");
+	audio.play();
+  }
+
+function countDown2(){
+	var currentVal = document.getElementById("countDownButton2").innerHTML;
+	var newVal = 0;
+
+	if (currentVal == 1){
+		revealMessage2()
+	}else if (currentVal > 0){
+
+		play()
+		newVal = currentVal - 1;	
+	}
+	document.getElementById('countDownButton2').innerHTML = newVal;
+
+
+}
+
