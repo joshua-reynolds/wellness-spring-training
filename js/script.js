abe126fdf26c980fa07d103d7c8734c7b056d085
Josh Reynolds <jreynolds@wfrc.org>
2023-02-15 14:36:10 -0700

    random batting function

--- js/script.js
+++ js/script.js
@@ -20,8 +20,18 @@ function revealMessage2() {
 	document.getElementById("hiddenMessage2").style.display = 'block';
 }
 
-function play() {
+function play0() {
+	var audio = new Audio("sound_effects/Laugh.mp3");
+	audio.play();
+  }
+
+function play1() {
 	var audio = new Audio("sound_effects/Baseball_Hit_Sound_Effect.mp3");
+	audio.play();
+  }
+  
+function play2() {
+	var audio = new Audio("sound_effects/Baseball_Hit_With_Cheer_Sound_Effect.mp3");
 	audio.play();
   }
 
@@ -39,5 +49,20 @@ function countDown2(){
 	document.getElementById('countDownButton2').innerHTML = newVal;
 
 
+}
+
+function bat(){
+	result = Math.floor(Math.random() * 5);
+	
+	if (result == 0){
+		play0()
+	}else if (result > 0 && result <= 2){
+		play1()	
+	}else if (result > 2){
+		play2()	
+	}
+	
+	const results = {0:'Out', 1:"Single", 2:"Double", 3:"Triple!", 4:"Home-Run!!"};
+	document.getElementById("bat_result").innerHTML = results[result];
 }
 
