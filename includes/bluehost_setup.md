a14330d1198d5361991426edad6bc0af12091921
Josh Reynolds <jdreynolds33@gmail.com>
2023-04-05 10:19:59 -0600

    add setup instructions

---
+++ includes/bluehost_setup.md
@@ -0,0 +1,5 @@
+# bluehost setup
+- pull any changes from github
+- create empty database, with new team names (sql for this is up a directory)
+- servername is localhost
+- input new db name, username and password
--- includes/dbconnect.inc.php
+++ includes/dbconnect.inc.php
@@ -1,8 +1,8 @@
 <?php
 
 $dbServername = "localhost";
-$dbUsername = "jreynolds";
-$dbPassword = 'popcorner';
+$dbUsername = "jreynolds"; // this is a dummy username
+$dbPassword = 'popcorner'; // this is a dummy password
 $dbName = 'wellness_spring_training'; // change this to new db every week
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
